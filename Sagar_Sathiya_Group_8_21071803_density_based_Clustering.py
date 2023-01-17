7bd4617f987590b6707c2107fdfffae69c2644db
ss22afk <ss22afk@herts.ac.uk>
2023-01-17 21:42:46 +0000

    DBSCAN algorithm completed.

--- Sagar_Sathiya_Group_8_21071803_density_based_Clustering.py
+++ Sagar_Sathiya_Group_8_21071803_density_based_Clustering.py
@@ -4,8 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import  NearestNeighbors #Importing a library for finding a nearest neighbors for DBSCAN.
 from sklearn.cluster import DBSCAN  # module from sklearn. cluster to perfome a density based clustering using DBSCAN alogorithm
-import seaborn as sns
+from sklearn.preprocessing import StandardScaler
 from kneed import KneeLocator
+import seaborn as sns
+from sklearn.metrics import silhouette_score
 import os
 
 def getDirecotry_path():
@@ -55,21 +57,25 @@ print('After column drop: ', breast_cancer_df.head(10))
 breast_cancer_df['diagnosis'] = breast_cancer_df['diagnosis'].map({'M':1, 'B':0})
 print('\n diagnosis value mapped: ', breast_cancer_df.head(10))
 
+# Extract columns from breast_cancer_df to process a clustering
+st = StandardScaler()
+breast_cancer_df_X = pd.DataFrame(st.fit_transform(breast_cancer_df), columns=breast_cancer_df.columns)
+#breast_cancer_df_X = breast_cancer_df.drop('diagnosis', axis=1).values
+#breast_cancer_df_X = StandardScaler().fit_transform(breast_cancer_df_X)
+
 # Summary statistics of breast cacncer db
 print('\n Discribed Breast cancer db: ', breast_cancer_df.describe())
 
 # Checking a shape of data
 print('\n Shape of data: ', breast_cancer_df.shape)
 
-# Extract columns from breast_cancer_df to process a clustering
-breast_cancer_df_X = breast_cancer_df.loc[:,['diagnosis','perimeter_mean']].values
 
 # shape of extracted dataframe
 print('Shape of extracted DF: ', breast_cancer_df_X.shape)
 
 '''
    To apply a DBSCAN algorithm we need neighborhood redius (eps) and minimum number of points (min_samples) as parameter.
-   we can Compute this parameter using nearestneighbors
+   we can Compute this parameter using nearestneighbors.
 '''
 # Creating an object of the NearestNeighbors class and fitting the data to the object
 '''
@@ -85,20 +91,47 @@ print('Distanced: and indices: ', distances, indices)
 distances = np.sort(distances[:10], axis=0)
 distances = distances[:,1]
 plt.figure(figsize=(5,5))
-plt.plot(distances)
+plt.plot(distances, label= 'data')
 plt.title('Distance variation at the 10th neighbour')
+plt.legend()
 plt.xlabel('Points')
 plt.ylabel('Distance')
 
 # KneeLocator to Detect Elbow point
 i = np.arange(len(distances))
 knee = KneeLocator(i, distances, S=1, curve='convex', direction='increasing', interp_method='polynomial')
-eps = float("{:.2f}".format(distances[knee.knee]))
-print('eps: ', eps) # eps:  0.35
+eps_cal = float("{:.2f}".format(distances[knee.knee]))
+print('eps_cal: ', eps_cal) # eps_cal:  2.7
 
 # plot destances with elbow point
-
 knee.plot_knee()
 plt.xlabel('Points')
 plt.ylabel('Distance')
+
+
+'''
+    Appling DBSCAN algorith on Breast cancer Dataset, ploting to scatter plot, calculate number of clusters.
+    eps_cal = 0.35
+    min_sample_pre = 10
+'''
+# Predicated value of min_sample from nearestneighbors
+min_sameple_pre = 10
+
+dbscan = DBSCAN(eps = eps_cal, min_samples = min_sameple_pre).fit(breast_cancer_df_X)
+breast_cancer_df_X_pred = dbscan.fit_predict(breast_cancer_df_X)
+labels = dbscan.labels_
+
+
+# Number of clusters in a labels, and noise
+n_clusters = len(set(labels))
+n_noise_ = list(labels).count(-1)
+
+print('Estimated number of clusters :', n_clusters)
+print('Estimated number of noise points:' , n_noise_)
+print(f"silhouette_score: {silhouette_score(breast_cancer_df_X,labels)}")
+
+
+# Scatter plot
+plt.figure(figsize=(5,5))
+sns.scatterplot(x=breast_cancer_df_X['diagnosis'], y=breast_cancer_df_X['perimeter_mean'], hue=labels, palette='viridis').set(title = 'cluster of diagnosis and perimeter_mean.')
 plt.show()
