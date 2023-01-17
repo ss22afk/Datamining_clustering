aedc7f6ed79706095b12ada3b5336c607cae43f0
ss22afk <ss22afk@herts.ac.uk>
2023-01-17 03:58:17 +0000

    Shape data and imoprt packages are done for DBSCAN

--- Sagar_Sathiya_Group_8_21071803_density_based_Clustering.py
+++ Sagar_Sathiya_Group_8_21071803_density_based_Clustering.py
@@ -2,7 +2,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import sklearn
+from sklearn.neighbors import  NearestNeighbors as nn #Importing a library for finding a nearest neighbors for DBSCAN.
+from sklearn.cluster import DBSCAN  # module from sklearn. cluster to perfome a density based clustering using DBSCAN alogorithm
+import seaborn as sns
 import os
 
 def getDirecotry_path():
@@ -25,12 +27,33 @@ def getDirecotry_path():
     
     return csv_file_path
     
-def readFilefromlocation(filename) :
+def readFileFromLocation(filename):
     '''
         This function is use for read file from "csvFiles" directory. 
         After a reading file processa load file and retrun a dataframe
         filename: Name of file as parameter.
     '''
-    
     dataframe = pd.read_csv(getDirecotry_path()+'/'+filename)
     return dataframe
+
+# Load a data from a CSVFiles Directory using readFilefrom readFileFromLocation()
+breast_Cancer_db = readFileFromLocation('BreastCancerDataSet.csv')
+print('Breast Cancer DB: ', breast_Cancer_db.head(10))
+
+# Check For missing value
+print('\n Total Count of Missing Value: ', breast_Cancer_db.isna().sum())
+
+# Information about Breast Cancer db
+print('\n Information about DB: ', breast_Cancer_db.info())
+
+# Drop a unneccessary column from a breast cancer db 
+breast_Cancer_db = breast_Cancer_db.drop(['id', 'Unnamed: 32'], axis=1)
+print('After column drop: ', breast_Cancer_db.head(10))
+
+# Summary statistics of breast cacncer db
+print('\n Discribed Breast cancer db: ', breast_Cancer_db.describe())
+
+# Checking a shape of data
+print('\n Shape of data: ', breast_Cancer_db.shape)
+
+
